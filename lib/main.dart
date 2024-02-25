c7f81687324a37450a52b972eafb8b3f38549237
unknown <zinebsebti2002@gmail.com>
2024-02-25 20:36:29 +0000

    listview of stadiums done, details view created not done yet

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CupConnect',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+       ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
